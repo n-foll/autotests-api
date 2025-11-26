b4f9eb35274cddaa83c50d61de4fadff9ff6fb4c
n-foll <n@foleeva.ru>
2025-11-26 20:05:52 +0300

    Практика работы с Faker

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -32,6 +32,6 @@ create_course_request = CreateCourseRequestSchema(
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
-create_exercise_request = CreateExerciseRequestSchema()
+create_exercise_request = CreateExerciseRequestSchema(course_id=create_course_response.course.id)
 create_exercise_response = exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
