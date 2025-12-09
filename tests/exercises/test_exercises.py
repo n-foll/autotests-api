493fb882dabf4e18ebffc39155bc7a580a6e6ff5
n-foll <n@foleeva.ru>
2025-12-09 19:09:06 +0300

    Практика написания Api ч3.3

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -4,10 +4,12 @@ from http import HTTPStatus
 
 from clients.exercises.exercises_client import ExercisesClient
 from fixtures.courses import CourseFixture
-from fixtures.exercises import ExerciseFixture
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, GetExerciseResponseSchema
+from fixtures.exercises import ExerciseFixture, exercises_client
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
+    assert_update_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -47,5 +49,22 @@ class TestExercises:
             actual_response=response_data,
             expected_response=function_exercise.response
         )
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+    def test_update_exercise (
+            self,
+            function_exercise: ExerciseFixture,
+            exercises_client: ExercisesClient
+    ):
+        request = UpdateExerciseRequestSchema()
+        response = exercises_client.update_exercise_api(function_exercise.response.exercise.id, request)
+
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        assert_update_exercise_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
