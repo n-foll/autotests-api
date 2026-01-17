e9dfa8adf52fb01400d3d135061589f66a0305ce
n-foll <n@foleeva.ru>
2026-01-18 01:00:59 +0300

    Настройки автотестов с Pydantic

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,12 +1,12 @@
-from functools import lru_cache  # Импортируем функцию для кеширования
+from functools import lru_cache
 
 from httpx import Client
 from pydantic import BaseModel
 
 from clients.authentication.authentication_client import get_authentication_client
-# Импортируем модель LoginRequestSchema
 from clients.authentication.authentication_schema import LoginRequestSchema
-
+from clients.event_hooks import curl_event_hook  # Импортируем event hook
+from config import settings
 
 # Добавили суффикс Schema вместо Dict
 class AuthenticationUserSchema(BaseModel, frozen=True):  # Добавили параметр frozen=True  # Наследуем от BaseModel вместо TypedDict
@@ -23,8 +23,9 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     login_response = authentication_client.login(login_request)
 
     return Client(
-        timeout=100,
-        base_url="http://localhost:8000",
+        timeout=settings.http_client.timeout,
+        base_url=settings.http_client.client_url,
         # Значения теперь извлекаем не по ключу, а через атрибуты
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+    event_hooks = {"request": [curl_event_hook]}  # Добавляем event hook для запроса
     )
